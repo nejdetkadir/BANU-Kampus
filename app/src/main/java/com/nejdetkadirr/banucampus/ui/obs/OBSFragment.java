f89376561759e7237c337123f1fb130dc5df2b34
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-20 04:02:22 +0300

    Add OBS page

--- app/src/main/java/com/nejdetkadirr/banucampus/ui/obs/OBSFragment.java
+++ app/src/main/java/com/nejdetkadirr/banucampus/ui/obs/OBSFragment.java
@@ -1,9 +1,14 @@
 package com.nejdetkadirr.banucampus.ui.obs;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,9 +16,25 @@ import androidx.fragment.app.Fragment;
 import com.nejdetkadirr.banucampus.R;
 
 public class OBSFragment extends Fragment {
+    private WebView obs_webView;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_obs, container, false);
+        obs_webView = rootView.findViewById(R.id.obs_webviewObs);
+        WebSettings webSettings= obs_webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        obs_webView.loadUrl("https://obs.bandirma.edu.tr");
+        obs_webView.setWebViewClient(new WebViewClient());
+        obs_webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == MotionEvent.ACTION_UP && obs_webView.canGoBack()) {
+                    obs_webView.goBack();
+                    return true;
+                }
+                return false;
+            }
+        });
         return rootView;
     }
 }
