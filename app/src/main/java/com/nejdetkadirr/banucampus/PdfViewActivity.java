b2412c56597a488f7f5fc251d67a37b4deccc5c7
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-21 00:48:52 +0300

    Add bus times

---
+++ app/src/main/java/com/nejdetkadirr/banucampus/PdfViewActivity.java
@@ -0,0 +1,35 @@
+package com.nejdetkadirr.banucampus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+
+public class PdfViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_view);
+        Intent intent = getIntent();
+        String fileName = intent.getStringExtra("fileName");
+        PDFView pdfView = findViewById(R.id.pdfview_pdfView);
+        pdfView.fromAsset(fileName)
+                .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+                .load();
+    }
+}
--- app/src/main/java/com/nejdetkadirr/banucampus/ui/bustimes/BusTimesFragment.java
+++ app/src/main/java/com/nejdetkadirr/banucampus/ui/bustimes/BusTimesFragment.java
@@ -1,19 +1,60 @@
 package com.nejdetkadirr.banucampus.ui.bustimes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.nejdetkadirr.banucampus.PdfViewActivity;
 import com.nejdetkadirr.banucampus.R;
 
-public class BusTimesFragment extends Fragment {
+import java.util.ArrayList;
 
+public class BusTimesFragment extends Fragment {
+    ArrayList<String> routes = new ArrayList<>();
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_bus_times, container, false);
+        addData();
+        ListView listView = rootView.findViewById(R.id.bus_times_listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), PdfViewActivity.class);
+                intent.putExtra("fileName","hat"+routes.get(position).split(" ")[0]+".pdf");
+                getActivity().startActivity(intent);
+            }
+        });
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity().getApplicationContext(),android.R.layout.simple_list_item_1,routes);
+        listView.setAdapter(arrayAdapter);
         return rootView;
+    }
+
+    public void addData() {
+        routes.add("1 NUMARA OTOGAR-ÇARŞI HATTI");
+        routes.add("2 NUMARA KONAKTUR HATTI");
+        routes.add("3 NUMARA KAYACIK TOKİ HATTI");
+        routes.add("4 NUMARA AYYILDIZ HATTI");
+        routes.add("5 NUMARA LEVENT HATTI");
+        routes.add("6 NUMARA YENİ MAHALLE - BEYAZ EVLER HATTI");
+        routes.add("7 NUMARA PAŞABAYIR HATTI");
+        routes.add("8 NUMARA 600 EVLER-SANAYİ HATTI");
+        routes.add("9 NUMARA LİVATYA HATTI");
+        routes.add("10 NUMARA FAKÜLTE-HASTANE HATTI");
+        routes.add("11 NUMARA PAŞABAYIR-HASTANE HATTI");
+        routes.add("12 NUMARA OKULLAR HATTI");
+        routes.add("14 LEVENT-HASTANE HATTI");
+        routes.add("15 NUMARA TOKİ-HASTANE HATTI");
+        routes.add("16 NUMARA VERGİ DAİRESİ HATTI");
+        routes.add("17 NUMARA 100. YIL KÜME EVLERİ HATTI");
+        routes.add("19 NUMARA BANVİT-TİCARET BORSASI HATTI");
+        routes.add("20 NUMARA AYYILDIZ-YENİ MAHALLE-HASTANE HATTI");
+        routes.add("21 NUMARA EDİNCİK-BANDIRMA HATTI");
     }
 }
