3e247858ee1c99a31d699cb2b56fbd541635781b
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-20 04:39:41 +0300

    Add progressbar to obs

--- app/src/main/java/com/nejdetkadirr/banucampus/ui/obs/OBSFragment.java
+++ app/src/main/java/com/nejdetkadirr/banucampus/ui/obs/OBSFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -17,14 +18,23 @@ import com.nejdetkadirr.banucampus.R;
 
 public class OBSFragment extends Fragment {
     private WebView obs_webView;
+    private ProgressBar obs_progressbar;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_obs, container, false);
         obs_webView = rootView.findViewById(R.id.obs_webviewObs);
+        obs_progressbar = rootView.findViewById(R.id.obs_progressBar);
+        obs_progressbar.setVisibility(View.VISIBLE);
         WebSettings webSettings= obs_webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         obs_webView.loadUrl("https://obs.bandirma.edu.tr");
-        obs_webView.setWebViewClient(new WebViewClient());
+        obs_webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                obs_progressbar.setVisibility(View.INVISIBLE);
+            }
+        });
         obs_webView.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
