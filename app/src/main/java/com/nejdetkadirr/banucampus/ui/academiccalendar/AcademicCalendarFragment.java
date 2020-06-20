b362fe0da4491ea3ae55c5216cc3fca32e576fdb
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-20 23:41:49 +0300

    Add academic calendar and implementation pdfview

--- app/src/main/java/com/nejdetkadirr/banucampus/ui/academiccalendar/AcademicCalendarFragment.java
+++ app/src/main/java/com/nejdetkadirr/banucampus/ui/academiccalendar/AcademicCalendarFragment.java
@@ -8,12 +8,29 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
 import com.nejdetkadirr.banucampus.R;
 
 public class AcademicCalendarFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_academic_calendar, container, false);
+        PDFView pdfView = rootView.findViewById(R.id.academic_calendar_pdfView);
+        pdfView.fromAsset("akademiktakvim.pdf")
+                .pages(0, 2, 1, 3, 3, 3) // all pages are displayed by default
+                .enableSwipe(true) // allows to block changing pages using swipe
+                .swipeHorizontal(false)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false) // render annotations (such as comments, colors or forms)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+                // spacing between pages in dp. To define spacing color, set view background
+                .spacing(0)
+                .pageFitPolicy(FitPolicy.WIDTH) // mode to fit pages in the view
+                .load();
         return rootView;
     }
 }
